6a24ab893a6e2a6a4a9872852ac9f2e46cced175
GabLeg <gab.legare@gmail.com>
2024-02-02 10:28:59 -0500

    refactor: update versions

--- src/rest-clients/chuck-jokes.client.ts
+++ src/rest-clients/chuck-jokes.client.ts
@@ -1,24 +1,25 @@
-import { Injectable } from "@nestjs/common";
-import { ChuckJoke } from "../domain/chuck";
-import { ConfigService } from "@nestjs/config";
-import { ChuckNorrisException } from "../controllers/exceptions";
-import { dtoToChuckJoke } from "./mappers";
-import { ChuckJokeDto } from "./dto";
-import { HttpService } from "@nestjs/axios";
+import {Injectable} from "@nestjs/common";
+import {ChuckJoke} from "../domain/chuck";
+import {ConfigService} from "@nestjs/config";
+import {ChuckNorrisException} from "../controllers/exceptions";
+import {dtoToChuckJoke} from "./mappers";
+import {ChuckJokeDto} from "./dto";
+import {HttpService} from "@nestjs/axios";
+import {firstValueFrom, map} from "rxjs";
 
 @Injectable()
 export class ChuckJokesClient {
 
-    constructor(private readonly httpService: HttpService,
-                private readonly configService: ConfigService) {
-    }
+  constructor(private readonly httpService: HttpService,
+              private readonly configService: ConfigService) {
+  }
 
-    async getJoke(): Promise<ChuckJoke> {
-        return this.httpService.get<ChuckJokeDto>(`${this.configService.get("CHUCK_JOKES_URL")}/jokes/random`)
-                   .toPromise()
-                   .then(response => dtoToChuckJoke(response.data))
-                   .catch(() => {
-                       throw new ChuckNorrisException();
-                   });
+  async getJoke(): Promise<ChuckJoke> {
+    try {
+      return firstValueFrom(this.httpService.get<ChuckJokeDto>(`${this.configService.get("CHUCK_JOKES_URL")}/jokes/random`)
+          .pipe(map(response => dtoToChuckJoke(response.data))))
+    } catch (error) {
+      throw new ChuckNorrisException()
     }
+  }
 }
